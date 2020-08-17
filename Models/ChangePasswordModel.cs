6bd35c6033e4fd27205f43ea314fbabe05205b3d
Константин Ян <konstantin.yan@gmail.com>
2020-08-17 15:22:03 +0300

    Active Directory users group

---
+++ Models/ChangePasswordModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiFactor.SelfService.Windows.Portal.Models
+{
+    public class ChangePasswordModel
+    {
+        [Required(ErrorMessage = "Пожалуйста, заполните")]
+        [DataType(DataType.Password)] 
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, заполните")]
+        [DataType(DataType.Password)]
+        public string PasswordAgain { get; set; }
+    }
+}
---
+++ Services/MultiFactorClaims.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiFactor.SelfService.Windows.Portal.Services
+{
+    public static class MultiFactorClaims
+    {
+        public const string SamlSessionId = "SamlSessionId";
+        public const string ChangePassword = "ChangePassword";
+    }
+}
