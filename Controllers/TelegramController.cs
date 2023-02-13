35a311f63dd5f21505b9ef1d6650b03fee5a5839
Aleksey Pashkov <pashkov.a.n@yandex.ru>
2023-02-13 12:31:17 +0600

    pass token to the view fix

--- Controllers/TelegramController.cs
+++ Controllers/TelegramController.cs
@@ -1,5 +1,6 @@
 ﻿using MultiFactor.SelfService.Windows.Portal.Attributes;
 using MultiFactor.SelfService.Windows.Portal.Core;
+using MultiFactor.SelfService.Windows.Portal.Services;
 using System.Web.Mvc;
 
 namespace MultiFactor.SelfService.Windows.Portal.Controllers
@@ -8,12 +9,21 @@ namespace MultiFactor.SelfService.Windows.Portal.Controllers
     public class TelegramController : ControllerBase
     {
         private readonly JwtTokenProvider _tokenProvider;
+        private readonly TokenValidationService _tokenValidationService;
 
-        public TelegramController(JwtTokenProvider tokenProvider)
+        public TelegramController(JwtTokenProvider tokenProvider, TokenValidationService tokenValidationService)
         {
             _tokenProvider = tokenProvider ?? throw new System.ArgumentNullException(nameof(tokenProvider));
+            _tokenValidationService = tokenValidationService ?? throw new System.ArgumentNullException(nameof(tokenValidationService));
         }
 
-        public ActionResult Index() => View(_tokenProvider.GetToken());       
+        public ActionResult Index()
+        {
+            if (_tokenValidationService.VerifyToken(_tokenProvider.GetToken(), out var parsedToken))
+            {
+                return View(parsedToken);
+            }
+            return SignOut();
+        }
     }
 }
