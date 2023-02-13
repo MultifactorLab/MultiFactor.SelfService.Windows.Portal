3a7d3bb8e53c477d033207abde19e43deafd3cea
Aleksey Pashkov <pashkov.a.n@yandex.ru>
2023-02-13 17:35:48 +0600

    added tests

---
+++ MultiFactor.SelfService.Windows.Portal.Tests/UITests/AccountTests.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace MultiFactor.SelfService.Windows.Portal.Tests.UITests
+{
+    public class AccountTests : AutomatedUITests
+    {
+        [Fact]
+        public void Login_Title_Success()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:49689");
+            Assert.StartsWith("Портал двухфакторной аутентификации", _driver.Title);
+        }
+        
+        [Fact]
+        public void Login_ValidCreds_Success()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:49689");
+
+            var login = _driver.FindElement(By.Id("UserName"));
+            var password = _driver.FindElement(By.Id("Password"));
+            var submit = _driver.FindElement(By.Id("submit"));
+
+            submit.Click();
+
+            var cookie = _driver.Manage().Cookies.GetCookieNamed("multifactor");
+            Assert.NotNull(cookie);
+
+            var form = _driver.FindElement(By.Id("remove-authenticator-form"));
+            Assert.NotNull(form);
+        }
+    }
+}
---
+++ MultiFactor.SelfService.Windows.Portal.Tests/UITests/AutomatedUITests.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiFactor.SelfService.Windows.Portal.Tests.UITests
+{
+    public class AutomatedUITests : IDisposable
+    {
+        protected readonly IWebDriver _driver;
+        public AutomatedUITests()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
