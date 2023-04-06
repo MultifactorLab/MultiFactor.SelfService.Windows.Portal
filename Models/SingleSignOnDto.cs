45b5070c1a3841b8d397534d970e8a3b970161d1
Oleg Brezhnev <oleg-423@yandex.ru>
2023-04-06 10:49:28 +0300

    SAML redirect fix

---
+++ Models/SingleSignOnDto.cs
@@ -0,0 +1,21 @@
+﻿
+using MultiFactor.SelfService.Windows.Portal.Services.API;
+using System.Text;
+
+namespace MultiFactor.SelfService.Windows.Portal.Models
+{
+    public class SingleSignOnDto
+    {
+        public bool HasSamlSession() => !string.IsNullOrEmpty(SamlSessionId);
+        public bool HasOidcSession() => !string.IsNullOrEmpty(OidcSessionId);
+        public string SamlSessionId { get; set; }
+        public string OidcSessionId { get; set; }
+        public override string ToString()
+        {
+            var sb = new StringBuilder(string.Empty);
+            if (HasSamlSession()) sb.Append($"?{MultiFactorClaims.SamlSessionId}={SamlSessionId}");
+            if (HasOidcSession()) sb.Append($"?{MultiFactorClaims.OidcSessionId}={OidcSessionId}");
+            return sb.ToString();
+        }
+    }
+}
