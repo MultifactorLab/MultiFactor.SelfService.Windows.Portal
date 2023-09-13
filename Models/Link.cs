3840d313f425a327f177aa71f7ef0a48e2bfc092
Oleg Brezhnev <oleg-423@yandex.ru>
2023-09-13 14:15:58 +0300

    link showcase added

---
+++ Models/Link.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MultiFactor.SelfService.Windows.Portal.Models
+{
+    public class Link
+    {
+        public Link(LinkElement linkElement) 
+        {
+            Url = linkElement.Url;
+            Title = linkElement.Title;
+            Image = linkElement.Image;
+        }
+
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
