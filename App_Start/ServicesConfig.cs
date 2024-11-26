276be1c8f9f81321219c2fcaefc5801d0667e927
Ivan-Timofeev <qwark4media@gmail.com>
2024-11-26 15:56:39 +0300

    remove unused files

--- App_Start/ServicesConfig.cs
+++ App_Start/ServicesConfig.cs
@@ -25,10 +25,7 @@ namespace MultiFactor.SelfService.Windows.Portal.App_Start
             services.AddTransient<Configure2FaController>();
             services.AddTransient<ExpiredPasswordController>();
             services.AddTransient<HomeController>();
-            services.AddTransient<MobileAppController>();
             services.AddTransient<PasswordController>();
-            services.AddTransient<TelegramController>();
-            services.AddTransient<TotpController>();
             services.AddTransient<ForgottenPasswordController>();
             services.AddTransient<ErrorController>();
             services.AddTransient<LdapConnectionFactory>();
