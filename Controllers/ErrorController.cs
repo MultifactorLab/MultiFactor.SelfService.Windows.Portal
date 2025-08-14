9cdc685df6266fdb59818c620aade0a376be0265
Sergey <42936486+Tu-S@users.noreply.github.com>
2025-08-14 15:41:56 +0700

    DEV-434 (#57)
    ### Release 14.08.2025 | Startup logs
    
    #### New
    
    - Added startup logger. Now startup errors are output to the console and written to the startup.log file. The file is located in the logs folder.
    - Improved antiforgery token error handling.

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -4,6 +4,7 @@ namespace MultiFactor.SelfService.Windows.Portal.Controllers
 {
     public class ErrorController : Controller
     {
-        public ActionResult Index() => View();      
+        public ActionResult Index() => View();
+        public ActionResult SessionExpired() => View();
     }
 }
